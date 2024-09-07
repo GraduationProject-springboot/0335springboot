27572a722acddf5881a4b382bd8a1ab1a934a663
陈奇 <chenqi2257@foryou56.com>
2024-09-07 11:49:46 +0800

    robot commit

--- src/main/java/com/service/WuzijuanzengService.java
+++ src/main/java/com/service/WuzijuanzengService.java
@@ -19,4 +19,25 @@ public interface WuzijuanzengService extends IService<WuzijuanzengEntity> {
     */
      PageUtils queryPage(Map<String, Object> params);
 
+}package com.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.utils.PageUtils;
+import com.entity.WuzijuanzengEntity;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.lang.Nullable;
+import java.util.List;
+
+/**
+ * 物资捐赠 服务类
+ */
+public interface WuzijuanzengService extends IService<WuzijuanzengEntity> {
+
+    /**
+    * @param params 查询参数
+    * @return 带分页的查询出来的数据
+    */
+     PageUtils queryPage(Map<String, Object> params);
+
 }
--- src/main/java/com/shequwuzijiaoyipingtaiApplication.java
+++ src/main/java/com/shequwuzijiaoyipingtaiApplication.java
@@ -21,3 +21,26 @@ public class shequwuzijiaoyipingtaiApplication extends SpringBootServletInitiali
         return applicationBuilder.sources(shequwuzijiaoyipingtaiApplication.class);
     }
 }
+package com;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@ServletComponentScan(value = "com.ServletContextListener")
+@MapperScan(basePackages = {"com.dao"})
+public class shequwuzijiaoyipingtaiApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(shequwuzijiaoyipingtaiApplication.class, args);
+	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(shequwuzijiaoyipingtaiApplication.class);
+    }
+}
